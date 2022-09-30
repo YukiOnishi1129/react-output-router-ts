a700ddf88bc37293e92ecae8d6ce13bf629aa6af
yukionishi1129 <yuki.onishi1129@gmail.com>
2022-09-30 16:19:15 +0900

    add: setup

---
+++ src/constants/data.ts
@@ -0,0 +1,19 @@
+/**
+ * data
+ *
+ * @package constants
+ */
+import {TodoType} from "../interfaces/Todo"
+
+export const INIT_TODO_LIST: Array<TodoType> = [
+    {
+        id: 1,
+        title: "Todo1",
+        content: "Todo1 content",
+    },
+    {
+        id: 2,
+        title: "Todo2",
+        content: "Todo2 content",
+    },
+]
